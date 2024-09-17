f3d3cecebffdf9c3909e0dd5ba353e63a7ac0052
Joel Gustafson <joelg@mit.edu>
2024-09-17 01:21:27 -0400

    control-panel: better stop behavior

--- control-panel/ApplicationWindow.zig
+++ control-panel/ApplicationWindow.zig
@@ -179,17 +179,11 @@ pub const ApplicationWindow = extern struct {
 
     fn handleStopClicked(_: *gtk.Button, win: *ApplicationWindow) callconv(.C) void {
         std.log.info("Stopping...", .{});
-        win.private().open_button.as(gtk.Widget).setSensitive(1);
-        win.private().save_button.as(gtk.Widget).setSensitive(1);
-        win.private().tick_button.as(gtk.Widget).setSensitive(1);
-        win.private().start_button.as(gtk.Widget).setSensitive(1);
-        win.private().stop_button.as(gtk.Widget).setSensitive(0);
-        win.private().randomize_button.as(gtk.Widget).setSensitive(1);
+
 
         win.private().status = .Stopped;
         if (win.private().engine_thread) |engine_thread| {
-            engine_thread.join();
-            win.private().engine_thread = null;
+            engine_thread.detach();
         }
     }
 
@@ -204,7 +198,7 @@ pub const ApplicationWindow = extern struct {
     }
 
     fn handleStartClicked(_: *gtk.Button, win: *ApplicationWindow) callconv(.C) void {
-        std.log.info("handleStartClicked", .{});
+        std.log.info("Starting...", .{});
 
         win.private().open_button.as(gtk.Widget).setSensitive(0);
         win.private().save_button.as(gtk.Widget).setSensitive(0);
@@ -264,7 +258,15 @@ pub const ApplicationWindow = extern struct {
             std.mem.writeInt(u64, msg.body()[0..8], engine.count, .big);
             try win.private().socket.send(msg, .{});
         }
+
         std.log.info("Stopped.", .{});
+
+        win.private().open_button.as(gtk.Widget).setSensitive(1);
+        win.private().save_button.as(gtk.Widget).setSensitive(1);
+        win.private().tick_button.as(gtk.Widget).setSensitive(1);
+        win.private().start_button.as(gtk.Widget).setSensitive(1);
+        win.private().stop_button.as(gtk.Widget).setSensitive(0);
+        win.private().randomize_button.as(gtk.Widget).setSensitive(1);
     }
 
     fn openFile(win: *ApplicationWindow, file: *gio.File) void {
